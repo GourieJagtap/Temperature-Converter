1ad065f22e78f12a9b588be55673639198368f79
Gourie <gouriejagtap@gmail.com>
2022-02-15 21:13:03 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var cel=document.getElementById('cel');
+var fah=document.getElementById('fah');
+var kel=document.getElementById('kel');
+
+cel.addEventListener('input', function(){
+    let c=this.value;
+    let f= (c*9/5)+32;
+    fah.value=f;
+    var k= Number(c) +Number(273.15) ;
+    kel.value=k;
+});
+fah.addEventListener('input', function(){
+    let f=this.value;
+    let c=(f-32)*5/9;
+    cel.value=c;
+    let k= (Number(f)-Number(273.15))*9/5;
+    kel.value=k;
+});
+kel.addEventListener('input', function(){
+    let k=this.value;
+    let f= (k*9/5)+32;
+    fah.value=f;
+    let c= Number(k)-Number(273.15);
+    cel.value=c;
+});
